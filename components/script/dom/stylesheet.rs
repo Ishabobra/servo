4db26d94246717ca6526cc284f25216398e614ba
Isha Bobra <ibobra@localhost.localdomain>
2016-03-15 16:57:17 -0400

    Initial work

---
+++ components/script/dom/stylesheet.rs
@@ -0,0 +1,41 @@
+use dom::ProcessingInstruction;
+use dom::bindings::reflector::Reflector;
+use util::str::DOMString;
+
+#[dom_struct]
+pub struct StyleSheet {
+    reflector_: Reflector,
+    type_: DOMString,
+    href: DOMString,
+    ownerNode: ProcessingInstruction,
+    parentStyleSheet: StyleSheet,
+    title: DOMString,
+    media: MediaList,
+    disabled: bool,
+}
+
+impl StyleSheetMethods for StyleSheet{
+    // http://dev.w3.org/csswg/cssom/#serialize-an-identifier
+    pub fn type_(&self)-> &DOMString{
+	&self.type_
+    }
+    pub fn href(&self)-> &DOMString{
+	&self.href
+    }
+    pub fn title(&self)-> &DOMString{
+	&self.title
+    }
+    pub fn parentStyleSheet(&self)-> StyleSheet{
+	&self.parentStyleSheet
+    }
+    pub fn media(&self)-> MediaList{
+	&self.media
+    }
+    pub fn disabled(&self)-> bool{
+	&self.disabled
+    }
+    pub fn ownerNode(&self)-> ProcessingInstruction{
+	&self.ownerNode
+    }
+}
+
